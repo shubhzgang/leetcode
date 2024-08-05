83291c97f9e176c330b955dcc961836f84190f13
Shubham Gangwal <shubhamgang123@gmail.com>
2024-08-05 14:58:17 +0000

    add 5th august solution

---
+++ N2053/Solution.java
@@ -0,0 +1,30 @@
+class Solution {
+    public String kthDistinct(String[] arr, int k) {
+        Set<String> distinct = new HashSet<>(), nonDistinct = new HashSet<>();
+
+        for (String s: arr) {
+            if (nonDistinct.contains(s)) {
+                continue;
+            } else if (distinct.contains(s)) {
+                nonDistinct.add(s);
+                distinct.remove(s);
+            } else {
+                distinct.add(s);
+            }
+        }
+        if (distinct.size() < k) {
+            return "";
+        }
+        int distinctCount = 0;
+        int i = 0;
+        for (; i < arr.length; i++) {
+            if (distinct.contains(arr[i])) {
+                distinctCount++;
+                if (distinctCount == k) {
+                    break;
+                }
+            }
+        }
+        return arr[i];
+    }
+}

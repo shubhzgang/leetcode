5f7f71554e1cf88b4b73d490394757089213a9e0
shubham gangwal <shgangwa@cisco.com>
2024-08-25 18:59:53 +0530

    reformat 100 solution

--- com/shubham/leetcode/N100/Solution.java
+++ com/shubham/leetcode/N100/Solution.java
@@ -3,16 +3,16 @@ package com.shubham.leetcode.N100;
 import com.shubham.leetcode.commons.TreeNode;
 
 class Solution {
-public boolean isSameTree(TreeNode p, TreeNode q) {
-    if ((p == null && q != null) || (q == null && p != null)) {
-        return false;
+    public boolean isSameTree(TreeNode p, TreeNode q) {
+        if ((p == null && q != null) || (q == null && p != null)) {
+            return false;
+        }
+        if (p == null && q == null) {
+            return true;
+        }
+        if (p.val != q.val) {
+            return false;
+        }
+        return isSameTree(p.left, q.left) && isSameTree(p.right, q.right);
     }
-    if (p == null && q == null) {
-        return true;
-    }
-    if (p.val != q.val) {
-        return false;
-    }
-    return isSameTree(p.left, q.left) && isSameTree(p.right, q.right);
-}
 }

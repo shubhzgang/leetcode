7174eadce0ab72c2d450f030af9b2c5a26ac6a74
Shubham Gangwal <shubhamgang123@gmail.com>
2024-10-15 09:17:16 +0530

    add 15th oct solution

---
+++ N2938/Solution.java
@@ -0,0 +1,21 @@
+package N2938;
+
+/**
+ * <a href="https://leetcode.com/problems/separate-black-and-white-balls/">2938. Separate Black and White Balls</a>
+ */
+class Solution {
+    public long minimumSteps(String s) {
+        long ans = 0;
+        int zi = 0;
+        char c;
+        for (int i = 0; i < s.length(); i++) {
+            c = s.charAt(i);
+            if (c == '0') {
+                ans += (i - zi);
+                zi++;
+            }
+        }
+        return ans;
+    }
+}
+// shubham gangwal
